4d0d97a0407c93249eec65d1da52d489034b64c2
Prashant <prashantd049@gmail.com>
2023-01-16 21:28:43 +0530

    Commits

--- src/Header.js
+++ src/Header.js
@@ -8,8 +8,7 @@ function Header() {
       <h1>Hello</h1>
 
         <div classname="header__left">
-                <img src="linkedin.png"
-                alt=""/>
+                <img src="https://www.flaticon.com/free-icon/linkedin_174857?term=linked+in&page=1&position=13&origin=search&related_id=174857"/>
 
                 <div classname="header__search">
                     <SearchIcon /> 

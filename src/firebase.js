f1becb8cb1cdf46db696138c3d343b3536135d6d
Prashant <prashantd049@gmail.com>
2023-01-17 22:57:54 +0530

    Commits

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCnGVzE9JzHM-EQB9YThK57TnkLPuPPkgk",
+    authDomain: "linkedin-clone-796db.firebaseapp.com",
+    projectId: "linkedin-clone-796db",
+    storageBucket: "linkedin-clone-796db.appspot.com",
+    messagingSenderId: "1043023993785",
+    appId: "1:1043023993785:web:72f9bec13ed8402838f468"
+  };
+  const firebaseApp = firebase.intializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};

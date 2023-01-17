065a98b7df752ebc0d77da6e2a7d2965c3498f7b
Prashant <prashantd049@gmail.com>
2023-01-17 11:26:49 +0530

    Commits

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -12,6 +12,19 @@ function Sidebar() {
             <h2>D Prashant</h2>
             <h4>prashantd049@gmail.com</h4>
         </div>
+        <div className= "sidebar__stats">
+            <div className="sidebar__stat">
+                <p>Who viewed you</p>
+                <p className="sidebar__number">2,543</p>
+            </div>
+            <div className="sidebar__stat">
+                <p>Views on Post</p>
+                <p className="sidebar__number">2,423</p>
+            </div>
+        </div>
+        <div className="sidebar__bottom">
+             <p>Recent</p>
+        </div>
     </div>
   )
 }

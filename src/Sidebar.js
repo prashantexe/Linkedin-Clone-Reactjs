68531637a94f3d35382660c40ae85b1b46636dab
Prashant <prashantd049@gmail.com>
2023-01-17 11:05:32 +0530

    Commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,17 @@
-import React from 'react;'
+import { Avatar } from '@mui/material';
+import React from 'react';
 import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      
+        <div className="sidebar__top">
+            
+            <img src="" alt= "" />
+            <Avatar />
+            <h2>D Prashant</h2>
+            <h4>prashantd049@gmail.com</h4>
+        </div>
     </div>
   )
 }

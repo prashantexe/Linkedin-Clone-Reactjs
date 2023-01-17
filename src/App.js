908228888301700a2a8339b9ae644b4e0693635d
Prashant <prashantd049@gmail.com>
2023-01-17 19:16:32 +0530

    Commits

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       {/* App Body */}
       <div className='app__body'>
         <Sidebar />
+        <Feed />
       </div>
 
     </div>

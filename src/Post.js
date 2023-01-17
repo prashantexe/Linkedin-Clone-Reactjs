e2b174006fe03eff00aac49059b57625cae12ff1
Prashant <prashantd049@gmail.com>
2023-01-17 20:39:46 +0530

    Commits

--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,20 @@
+import { Avatar } from "@mui/icons-material"
 import React from 'react';
 import "./Post.css";
 
-function Post() {
+function Post({name, description, message, photoUrl}) {
   return (
     <div className="post">
-      
+        <div className="post__header">
+            <Avatar />
+            <div className="post__info">
+                <h2>D Prashant</h2>
+                <p>Description</p>
+            </div>
+        </div>
+      <div className="post__body">
+        <p>Message goes here</p>
+      </div>
     </div>
   )
 }

af88a475303f0cd631ab17b4287e57d62771614b
Prashant <prashantd049@gmail.com>
2023-01-18 17:45:27 +0530

    Commits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import { useDispatch, useSelector} from "react-redux";
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import {selectUser} from "./features/counter/userSlice";
 import Feed from "./Feed";
 import Login from "./Login";
+import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect (() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        // user is logged in
+      } else{
+        // user is logged out
+        dispatch(logout());
+      }
+    })
+  }, [])
+
+
 
   return (
     <div className="app">

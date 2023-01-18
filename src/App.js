949e02c9e8830be9a40161b9509ef00e5077a026
Prashant <prashantd049@gmail.com>
2023-01-18 18:45:36 +0530

    Revert "Commits"
    This reverts commit ed1ca1f8ae2a212e5ee0108006766244151fedb2.

--- src/App.js
+++ src/App.js
@@ -3,33 +3,33 @@ import { useDispatch, useSelector} from "react-redux";
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
-// import {login, logout, selectUser} from "./features/counter/userSlice";
+import {login, logout, selectUser} from "./features/counter/userSlice";
 import Feed from "./Feed";
-// import Login from "./Login";
+import Login from "./Login";
 import { auth } from './firebase';
 import { useEffect }from 'react';
 
 function App() {
   const user = useSelector(selectUser);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect (() => {
-  //   auth.onAuthStateChanged(userAuth => {
-  //     if(userAuth) {
-  //       // user is logged in
-  //       dispatch(login({
-  //         email: userAuth.email,
-  //         uid: userAuth.uid,
-  //         displayName: userAuth.displayName,
-  //         photoUrl: userAuth.photoURL,
-  //       })
-  //       );
-  //     } else{
-  //       // user is logged out
-  //       dispatch(logout());
-  //     }
-  //   });
-  // }, []);
+  useEffect (() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        // user is logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        })
+        );
+      } else{
+        // user is logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
 
 
 
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import userReducer from '../features/counter/userSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../features/counter/userSlice';
 
-// export const store = configureStore({
-//   reducer: {
-//     user: userReducer,
-//   },
-// },+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+},+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);

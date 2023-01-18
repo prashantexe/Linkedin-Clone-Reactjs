215854e19c7bb585d4f8b8da0972e0accfa1ac1e
Prashant <prashantd049@gmail.com>
2023-01-18 16:10:58 +0530

    Commits

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import {useSelector} from "react-redux";
 import './App.css';
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import {selectUser} from "./features/counter/userSlice";
 import Feed from "./Feed";
-
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
 
-      {/* App Body */}
+    {!user ? (
+      <Login />
+    ): (
+
       <div className='app__body'>
         <Sidebar />
         <Feed />
       </div>
-
+    )}
     </div>
   );
 }

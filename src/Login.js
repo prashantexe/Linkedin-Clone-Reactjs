f89fd8579d5cbba02afcc2a22e0d7cf2e13f302a
Prashant <prashantd049@gmail.com>
2023-01-18 18:38:33 +0530

    Commits

--- src/Login.js
+++ src/Login.js
@@ -1,63 +1,64 @@
-import React, {useState} from 'react';
-import "./Login.css";
-import {auth} from "./firebase"; 
-import { useDispatch } from 'react-redux';
-import {login} from "./features/counter/userSlice"; 
+// import React, {useState} from 'react';
+// import "./Login.css";
+// import {auth} from "./firebase"; 
+// import { useDispatch } from 'react-redux';
+// import {login} from "./features/counter/userSlice"; 
 
 
-function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
-    const [profilePic, setProfilePic] = useState("");
-    const  dispatch = useDispatch();
+// function Login() {
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//     const [name, setName] = useState("");
+//     const [profilePic, setProfilePic] = useState("");
+//     const  dispatch = useDispatch();
 
-    const logintoApp = (e) => {
-            e.preventDefault();
-    };
+//     const logintoApp = (e) => {
+//             e.preventDefault();
+//     };
 
-    const register = () => {
-        if(!name){
-            return alert ("Please enter a full Name!");
-        }
-            auth.createUserWithEmailAndPassword(email, password)
-            .then ((userAuth) => {
-                userAuth.user.updateProfile({
-                    displayName: name,
-                    photoURL: profilePic,
-                })
-                .then(()=> {
-                       dispatch(login({
-                            email: userAuth.user.email,
-                            uid: userAuth.user.uid,
-                            displayName: name,
-                            photoURL: profilePic
-                       })
-                    ); 
-                });
-            });
-    };
+//     const register = () => {
+//         if(!name){
+//             return alert ("Please enter a full Name!");
+//         }
+//             auth.createUserWithEmailAndPassword(email, password)
+//             .then ((userAuth) => {
+//                 userAuth.user.updateProfile({
+//                     displayName: name,
+//                     photoURL: profilePic,
+//                 })
+//                 .then(()=> {
+//                        dispatch(login({
+//                             email: userAuth.user.email,
+//                             uid: userAuth.user.uid,
+//                             displayName: name,
+//                             photoURL: profilePic
+//                        })
+//                     ); 
+//                 });
+//             })
+//             .catch((error) => alert (error.message));
+//     };
 
-  return (
-    <div className ="login">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
+//   return (
+//     <div className ="login">
+//         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
 
-    <form>
-        <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Full name (required if registering)" type="text" />
+//     <form>
+//         <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Full name (required if registering)" type="text" />
 
-        <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder="Profile pic URL (Optional)" type="text" />
+//         <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder="Profile pic URL (Optional)" type="text" />
 
-        <input value ={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" />
-        <input value ={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
-        <button type="submit" onClick={logintoApp}>Sign In</button>
+//         <input value ={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" type="email" />
+//         <input value ={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" type="password" />
+//         <button type="Submit" onClick={logintoApp}>Sign In</button>
 
-    </form>
+//     </form>
 
-    <p> Not a member?? {" "}
-        <span className ="login__register" onClick={register}>Register Now!!</span>
-    </p>
-     </div>
-  )
-}
+//     <p> Not a member?? {" "}
+//         <span className ="login__register" onClick={register}>Register Now!!</span>
+//     </p>
+//      </div>
+//   )
+// }
 
-export default Login
+// export default Login

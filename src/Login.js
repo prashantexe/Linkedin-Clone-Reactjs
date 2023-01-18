fbe782e34fcdeacb18c42cf00b7d1f6c99919355
Prashant <prashantd049@gmail.com>
2023-01-18 16:26:47 +0530

    Commits

--- src/Login.js
+++ src/Login.js
@@ -2,10 +2,31 @@ import React from 'react';
 import "./Login.css";
 
 function Login() {
+
+    const register = () => {};
   return (
     <div className ="login">
-      
-    </div>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinkedIn_Logo.svg/1200px-LinkedIn_Logo.svg.png" alt="" />
+
+    <form>
+        <input placeholder="Full name (required if registering)"
+        type="text" />
+
+        <input placeholder="Profile pic URL (Optional)"
+        type="text" />
+
+        <input placeholder="Email"
+        type="email" />
+        <input placeholder="Password"
+        type="password" />
+        <button>Sign In</button>
+
+    </form>
+
+    <p> Not a member??
+        <span className ="login__register" onClick={register}>Register Now!!</span>
+    </p>
+     </div>
   )
 }
 

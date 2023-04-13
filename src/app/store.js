f477c9eb949d5a6648081635906c2cd282b427ec
Prashant <prashantd049@gmail.com>
2023-04-13 20:12:49 +0530

    Commit

--- src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/counter/userSlice';
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-},+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
